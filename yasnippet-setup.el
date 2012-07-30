cf50ae5279edc7ffa125affac9fbf1ca3a21f680
Łukasz Dubiel <bambucha14@gmail.com>
2012-07-30 17:49:23 +0200

    Add list of TODO for config and other thing around that editor.

--- yasnippet-setup.el
+++ yasnippet-setup.el
@@ -1,7 +1,8 @@
 (require 'custom-load-path)
 
 (require 'yasnippet)
-(setf yas/snippet-dirs  (list "~/emacs.d/yasnippet/snippets/"))
+
+(setf yas/snippet-dirs  (list "~/.emacs.d/yasnippet/snippets/"))
 (yas/global-mode 1)
 (global-set-key (kbd "C-c TAB") 'yas/expand)
 
