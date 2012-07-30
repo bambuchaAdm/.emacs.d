af74ae1d80f99c3ccff1c8409b53123ff0452108
Łukasz Dubiel <bambucha14@gmail.com>
2012-07-30 23:01:36 +0200

    Flymake-mode for python work now good.

--- flymake-python.el
+++ flymake-python.el
@@ -6,16 +6,16 @@
 	 (local-file (file-relative-name
 		      temp-file
 		      (file-name-directory buffer-file-name))))
-    (list "nosetests" (list "-v" "1"))))
+    (list "python" (list local-file))))
 
 
 
 (add-to-list 'flymake-allowed-file-name-masks
  '(".+\\.py$" flymake-python-init flymake-simple-cleanup flymake-get-real-file-name))
 
-;(setq flymake-err-line-patterns
-;      (cons '("\\(.*\\) at \\([^ \n]+\\) line \\([0-9]+\\)[,.\n]"
-;	      2 3 nil 1)
-;	    flymake-err-line-patterns))
+;file line column err-text-idx
+(add-to-list 'flymake-err-line-patterns
+     '("File \"\\(.*\\)\", line \\(.*\\), in \\(.*\\(:?\n.*\n\\).*\\)" 1 2 nil 3))
+
 
 (provide 'flymake-python)
