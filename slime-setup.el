fa7bf8bd2394df4df0e6376719d9c63c12ea5834
Łukasz Dubiel <bambucha14@gmail.com>
2012-07-30 17:32:01 +0200

    Remove redundant path in load-path for slime

--- slime-setup.el
+++ slime-setup.el
@@ -1,6 +1,5 @@
 (require 'custom-load-path)
 
-(add-to-list 'load-path (system-lisp-subdirecotry "slime"))
 (setq inferior-lisp-program "/usr/bin/sbcl")
 (require 'slime)
 (slime-setup '(slime-fancy))
