18db6ad290dea3c17660a0f38450e7e7c5c3e368
Łukasz Dubiel <bambucha14@gmail.com>
2012-07-31 08:09:32 +0200

    Add fullscreen state to fullscreen.el
    Now will not minimaize when load two times.

--- fullscreen.el
+++ fullscreen.el
@@ -1,9 +1,12 @@
+(defvar *fullscreen-is-maximaliezd* nil)
+
 (defun toggle-fullscreen ()
   (interactive)
   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+  (setq *fullscreen-is-maximaliezd* (not *fullscreen-is-maximaliezd*)))
 
-(when window-system
+(when (and window-system (not *fullscreen-is-maximaliezd*))
   (toggle-fullscreen))
 
 (provide 'fullscreen)
