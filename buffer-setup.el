1cdca3ffd2fe58b3172d589e80668489e8b1ff0b
Łukasz Dubiel <bambucha14@gmail.com>
2012-07-31 08:22:34 +0200

    Fix C-x C-b to run bs-show

--- buffer-setup.el
+++ buffer-setup.el
@@ -1,12 +1,11 @@
 (require 'uniquify)
 
-(message "%s" "Ladowanie ustawien bufforow")
 (iswitchb-mode 1)
 (show-paren-mode 1)
 (setf uniquify-buffer-name-style 'post-forward)
 
+(require 'bs)
 (global-set-key (kbd "C-c C-b") #'bs-show)
-
 
 (provide 'buffer-setup)
 
