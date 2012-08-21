f1995447df5bdb10e62c6de0a2002acd93100f33
Łukasz Dubiel <bambucha14@gmail.com>
2012-08-21 11:59:28 +0200

    Now compilation window focus on fail.

--- compile-setup.el
+++ compile-setup.el
@@ -3,20 +3,21 @@
 (define-key compilation-mode-map (kbd "q") #'delete-window)
 (global-set-key (kbd "C-c C-m") #'recompile)
 
+(defun take-focus-on-fail-compilation (buffer message)
+  (when (string-match-p "abnormally" message)
+    (select-window (get-buffer-window buffer))))
+(add-hook 'compilation-finish-functions #'take-focus-on-fail-compilation)
+
 (defun close-compilation-window-when-success (buffer)
   (let ((window (get-buffer-window buffer)))
     (when window
       (delete-window window))))
 
 (defun layzzy-close-compilation-window (buffer message)
-  (when (string-match-p "finished" message)
-    (run-with-timer 1
-		    nil
-		    (apply-partially #'close-compilation-window-when-success buffer))
-  (message "Timer started")))
-
+  (let ((function (apply-partially #'close-compilation-window-when-success buffer)))
+    (when (string-match-p "finished" message)
+      (run-with-timer 1 nil function))))
 (add-hook 'compilation-finish-functions #'layzzy-close-compilation-window)
-
 
 (provide 'compile-setup)
 
