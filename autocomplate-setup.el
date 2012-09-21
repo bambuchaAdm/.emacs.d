c7dd0d5c4e788b8772ceb70d72e521265cf23cb2
Łukasz Dubiel <bambucha14@gmail.com>
2012-09-21 20:28:43 +0200

    Auto-complete update to master

--- autocomplate-setup.el
+++ autocomplate-setup.el
@@ -5,4 +5,7 @@
 (global-set-key (kbd "<backtab>") #'auto-complete)
 (setq-default ac-sources (append '(ac-source-yasnippet) ac-sources))
 
+(setq ac-use-menu-map t
+	  ac-use-fuzzy t)
+
 (provide 'autocomplate-setup)
