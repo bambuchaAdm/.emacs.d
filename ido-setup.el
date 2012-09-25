26ade4864f7492d8746cdf12ff69e348f6ab2eda
?ukasz Dubiel <bambucha14@gmail.com>
2012-09-25 23:28:48 +0200

    Add fuzzy-fiond and other configuration option to IDO-mode

--- ido-setup.el
+++ ido-setup.el
@@ -1,6 +1,12 @@
-(require 'ido-ubiquitous)
-
+(require 'ido)
 (ido-mode 1)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point nil
+      ido-max-prospects 10)
+
+(require 'ido-ubiquitous)
 (ido-ubiquitous-mode 1)
 
 (provide 'ido-setup)
