e8001c8d7fc8a3f84dedb6c487c8985ece3ccab1
?ukasz Dubiel <bambucha14@gmail.com>
2012-09-25 23:23:46 +0200

    Add ruby test-source jumping function

--- ruby-setup.el
+++ ruby-setup.el
@@ -1,5 +1,29 @@
 (require 'ruby-mode)
 
+(defun ruby--jump-to-test ()
+  (find-file
+   (replace-regexp-in-string
+    "/lib/" "/test/"
+    (replace-regexp-in-string
+     "/\\([^/]+\\).rb$" "/test_\\1.rb"
+     (buffer-file-name)))))
+
+(defun ruby--jump-to-lib ()
+  (find-file
+   (replace-regexp-in-string
+    "/test/" "/lib/"
+    (replace-regexp-in-string
+     "/test_\\([^/]+\\).rb$" "/\\1.rb"
+     (buffer-file-name)))))
+
+(defun ruby-jump-to-other ()
+  (interactive)
+  (if (string-match-p "/test/" (buffer-file-name))
+      (ruby--jump-to-lib)
+    (ruby--jump-to-test)))
+
+(define-key ruby-mode-map (kbd "C-c j") 'ruby-jump-to-other)
+
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.watchr$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
