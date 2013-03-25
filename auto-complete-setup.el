b529f13dbc2363d9d6218f719e11d0f3c8491014
Łukasz Dubiel <bambucha14@gmail.com>
2013-03-25 09:18:28 +0100

    Add config from auto-complete config in setup file. No more magic fucntion. Known source and comfiguration.
    Add scss-mode to support Rails projects.

--- auto-complete-setup.el
+++ auto-complete-setup.el
@@ -1,10 +1,15 @@
 (require 'auto-complete-config)
 
-(ac-config-default)
+(setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers ac-source-yasnippet))
+(add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+(add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+(add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+(add-hook 'css-mode-hook 'ac-css-mode-setup)
+(add-hook 'auto-complete-mode-hook 'ac-common-setup)
+(global-auto-complete-mode t)
 
 (global-set-key (kbd "<backtab>") #'auto-complete)
-(setq-default ac-sources (append '(ac-source-yasnippet) ac-sources))
-
+ 
 (setq ac-use-menu-map t
 	  ac-use-fuzzy t)
 
