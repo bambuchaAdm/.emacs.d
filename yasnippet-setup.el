e679c3a114d4d69a416a1f6d0ea3a8b0bf45c7f9
Łukasz Dubiel <bambucha14@gmail.com>
2013-03-31 13:23:04 +0200

    Update in submodules.
    Update form deprecated functions.

--- yasnippet-setup.el
+++ yasnippet-setup.el
@@ -3,8 +3,8 @@
 (require 'yasnippet)
 
 (setq yas-trigger-key nil
-	  yas/triggers-in-field t
-	  yas/snippet-dirs (list (create-directory-repository-path "snippets")))
+	  yas-triggers-in-field t
+	  yas-snippet-dirs (list (create-directory-repository-path "snippets")))
 (yas-reload-all)
 (yas-global-mode 1)
 
