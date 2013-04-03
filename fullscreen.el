bcc019baaebfebc43a58a767cea8534aaaa9c8d6
Łukasz Dubiel <bambucha14@gmail.com>
2013-04-03 02:49:21 +0200

    Add fullscreen bind to F11.

--- fullscreen.el
+++ fullscreen.el
@@ -6,6 +6,8 @@
   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
   (setq *fullscreen-is-maximaliezd* (not *fullscreen-is-maximaliezd*)))
 
+(global-set-key (kbd "<f11>") 'toggle-fullscreen)
+
 (when (and window-system (not *fullscreen-is-maximaliezd*))
   (toggle-fullscreen))
 
